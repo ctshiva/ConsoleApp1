fb96466d6a397cc6590008132ebd70101856a806
Iyer <SIyer@ncua.gov>
2019-01-08 13:20:58 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string accountCode = "AS0048";
+            string accountCode = "1011A";
+            if (!accountCode.StartsWith("A") && !accountCode.StartsWith("V"))
+            {
+                accountCode = "A" + accountCode;
+            }
+        }
+    }
+}
